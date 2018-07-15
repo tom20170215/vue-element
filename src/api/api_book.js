386751987c06115847608fa02283539b5e359aef
tom20170215 <tom_tyceshi@163.com>
2018-07-16 07:37:58 +0800

    20180716

--- src/api/api_book.js
+++ src/api/api_book.js
@@ -5,5 +5,10 @@ export default {
   // 查询获取book列表
   findList: params => {
     return API.GET('/api/v1/books', params)
+  },
+  // 批量删除，传数组
+  removeBatch: ids => {
+    return API.DELETE(`/api/v1/books/batch/${ids}`)
   }
+
 }
--- src/components/book/list.vue
+++ src/components/book/list.vue
@@ -23,7 +23,7 @@
                 </el-form>
             </el-col>
             <!-- 列表 -->
-            <el-table :data="books" highlight-current-row style="width:100%;">
+            <el-table :data="books" highlight-current-row style="width:100%;" @selection-change="selschange">
                 <el-table-column type="selection" width="55"></el-table-column>
                 <el-table-column type="index" width="69"></el-table-column>
                 <el-table-column type="expand">
@@ -45,11 +45,32 @@
                     </template>
                 </el-table-column>
             </el-table>
-            <!-- 工具条 -->
+            <!-- 底部工具条 -->
             <el-col :span="24" style="margin-top:50px;">
-                <el-button type="danger">批量删除</el-button>
-                <el-pagination layout="prev, pager, next" :page-size="10" :total="total" background style="float:right;"></el-pagination>
+                <el-button type="danger" :disabled="this.sels.length === 0" @click.native="deleteBook">批量删除</el-button>
+                <el-pagination layout="prev, pager, next" :page-size="10" :total="total" background style="float:right;" @current-change="handleCurrentChange"></el-pagination>
             </el-col>
+            <!-- 新增界面 -->
+            <el-dialog title="新增" :visible.sync='addFormVisible' :close-on-click-modal='false'>
+                <el-form :modal="addForm" label-width="80px" :rules="addFormRules" ref="addForm"> 
+                    <el-form-item label="书名" prop="name">
+                        <el-input v-model="addForm.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="作者" prop="author">
+                        <el-input v-model="addForm.author"></el-input>
+                    </el-form-item>
+                    <el-form-item label="出版日期" prop="publishAt">
+                        <el-date-picker type="date" placeholder="选择日期" v-model="addForm.publishAt"></el-date-picker>
+                    </el-form-item>
+                    <el-form-item label="简介" prop="description">
+                        <el-input type="textarea" :rows="8" v-model="addForm.description"></el-input>
+                    </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer">
+                    <el-button @click.native="addFormVisible = false">取消</el-button>
+                    <el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+                </div>
+            </el-dialog>
         </el-col>
     </el-row>
 </template>
@@ -61,11 +82,34 @@ import API from '../../api/api_book.js'
             return {
                 loading: false,
                 filters: {
-                    name: ''
+                    name: '' // 查询的书名
                 },
-                books: [],
-                total: 0,
-                page: 1
+                books: [], // 图书列表
+                total: 0,  
+                page: 1,
+                sels: [], // 被选中的图书数组
+                addLoading: false,
+                addFormVisible: true,
+                addForm: {
+                    name: '',
+                    author: '',
+                    publishAt: '',
+                    description: ''
+                },
+                addFormRules: {
+                    name: [
+                        {required: true, message: '请输入书名', trigger: 'blur'}
+                    ],
+                    author: [
+                        {required: true, message: '请输入作者', trigger: 'blur'}
+                    ],
+                    publishAt: [
+                        {required: true, message: '请输入日期', trigger: 'change'}
+                    ],
+                    description: [
+                        {required: true, message: '请输入简介', trigger: 'blur'}
+                    ]
+                }
             }
         },
         methods: {
@@ -97,6 +141,40 @@ import API from '../../api/api_book.js'
                 });
             },
             showAddDialog() {
+
+            },
+            handleCurrentChange(val) {
+                this.page = val;
+                this.search();
+            },
+            selschange(val) {
+                this.sels = val
+            },
+            // 批量删除
+            deleteBook() {
+                let ids = this.sels.map(item => item.id).toString();
+                let that = this;
+                this.$confirm('确认删除选中记录吗？','提示',{
+                    type: 'warning'
+                }).then((() => {
+                    that.loading = true;
+                    API.removeBatch(ids).then(function (result) {
+                        that.loading = false;
+                        if (result && parseInt(result.errcode) === 0) {
+                            that.$message.success({showClose: true, message: '删除成功', duration: 1500});
+                            that.search();
+                        }
+                    }, function (err) {
+                        that.loading = false;
+                        that.$message.error({showClose: true, message: err.toString(), duration: 1500});
+                    }).catch(function (error) {
+                        that.loading = false;
+                        that.$message.error({showClose: true, message: '请求出现异常', duration: 1500});
+                    });
+                }))
+            },
+            // 新增图书
+            addSubmit() {
 
             }
         },
